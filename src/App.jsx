3e1725d74eee53a523e90c842325464d6a1aa426
Beimbet <beimbet.maratov@mail.ru>
2024-03-04 05:05:08 +0500

    Something changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import Nav from "./Nav.jsx";
 import './scripts.js'
 // import MainPage from "./pages/MainPage/MainPage.jsx"
 import Footer from "./Footer.jsx"
-import DeliveryPage from "./pages/Delivery/DeliveryPage.jsx";
+// import DeliveryPage from "./pages/Delivery/DeliveryPage.jsx";
 // import CablePage from "./pages/CablePage/CablePage.jsx";
+import HowToBuyPage from "./pages/HowToBuyPage/HowToBuyPage.jsx";
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
     <>
         <Nav />
         <div className="wrapper">
-            <DeliveryPage/>
+            <HowToBuyPage/>
         </div>
         <Footer />
     </>
