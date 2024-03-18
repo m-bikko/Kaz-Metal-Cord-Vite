c859fd99dfc7674aca604ee643fb10d6a2f7c407
Bolat Abdulaziz <Bola_cg56@student.itstep.kz>
2024-03-18 22:52:56 +0300

    Changes

--- src/pages/CablePages/ProvodaSilovyePage.jsx
+++ src/pages/CablePages/ProvodaSilovyePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import cardData from '../../../../Kaz-Metal-Cord-Vite/src/components/Shared/jsons/provoda-silovye/provoda-silovye.json';
-import filterCardData from '../../../../Kaz-Metal-Cord-Vite/src/components/Shared/jsons/provoda-silovye/provoda-silovye-filter.json'
+import cardData from '../../components/Shared/jsons/provoda-silovye/provoda-silovye.json';
+import filterCardData from '../../components/Shared/jsons/provoda-silovye/provoda-silovye-filter.json'
 import CablePage from "../../components/CablePage/CablePage";
 import image from '../../photos/products/провода-силовые.png'
 function ProvodaSilovyePage() {
--- src/pages/CablePages/SoedinitelnyeProvodaPage.jsx
+++ src/pages/CablePages/SoedinitelnyeProvodaPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import cardData from '../../../../Kaz-Metal-Cord-Vite/src/components/Shared/jsons/soedinitelnye-provoda/soedinitelnye-provoda.json';
-import filterCardData from '../../../../Kaz-Metal-Cord-Vite/src/components/Shared/jsons/soedinitelnye-provoda/soedinitelnye-provoda-filter.json'
+import cardData from '../../components/Shared/jsons/soedinitelnye-provoda/soedinitelnye-provoda.json';
+import filterCardData from '../../components/Shared/jsons/soedinitelnye-provoda/soedinitelnye-provoda-filter.json'
 import CablePage from "../../components/CablePage/CablePage";
 import image from '../../photos/products/соединительные-провода.png'
 function SoedinitelnyeProvodaPage() {
